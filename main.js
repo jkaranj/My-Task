c6900461fa1f508ccad2a4b7b475b40198a24bbc
karan <jkaran1998@gmail.com>
2020-04-30 14:39:25 +0530

    getting input

--- main.js
+++ main.js
@@ -2,3 +2,21 @@ const dateElement = document.getElementById("date");
 const option = {weekday : "long", month : "short", day : "numeric" };
 const today = new Date();
 dateElement.innerHTML = today.toLocaleDateString("en-us", option);
+
+document.getElementById("icon").addEventListener("click", () => {
+    if(document.getElementById("note").value == ""){
+        alert("This field can't be empty")
+        return false;
+    }
+    const note = document.getElementById("note").value
+    console.log(note);
+    const cat = document.getElementById("catt").value
+    console.log(cat);
+    
+})
+document.getElementById("note").addEventListener("keyup", function(event) {
+    if(event.keycode === 13){
+        event.preventDefault();
+        document.getElementById("icon").click();
+    }
+})
